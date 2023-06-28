1e62c45b5acb3cd89679f16493abf4c75bb93b49
Philipp Mildenberger <philipp@mildenberger.me>
2023-06-28 19:06:34 +0200

    Added another blocks examples (similar as the tui-rs one)

---
+++ examples/blocks.rs
@@ -0,0 +1,29 @@
+use anyhow::Result;
+use ratatui::style::{Color, Style};
+use trui::*;
+
+// TODO layouting is not really optimal yet (needs to be configurable, a good abstraction on top of taffy likely)
+fn main() -> Result<()> {
+    App::new((), |()| {
+        block(h_stack((
+            v_stack((
+                block(("With".fg(Color::Yellow), " background").wrapped()).bg(Color::LightYellow),
+                block("text inside block").with_borders(BorderKind::Straight),
+            )),
+            v_stack((
+                block("Styled title".bg(Color::Red).fg(Color::White)).bg(Color::LightCyan),
+                block(v_stack((
+                    "With styled borders and doubled borders",
+                    block("Block inside block").with_borders(BorderKind::Straight),
+                )))
+                .with_borders((
+                    Borders::VERTICAL,
+                    Style::default().fg(Color::Cyan),
+                    BorderKind::DoubleStraight,
+                )),
+            )),
+        )))
+        .with_borders(BorderKind::Rounded)
+    })
+    .run()
+}
